3618e02a2a1c40715cda0f541b57eee23682f2c1
mcantero15 <mariana.a.cantero15@gmail.com>
2023-09-22 15:10:39 -0400

    Initial Commit

---
+++ BluePhone/BluePhoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  BluePhoneApp.swift
+//  BluePhone
+//
+//  Created by Mariana Cantero on 9/22/23.
+//
+
+import SwiftUI
+
+@main
+struct BluePhoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
