8c6a697372e1407422e675cad240cd0b173683e7
mcantero15 <mariana.a.cantero15@gmail.com>
2024-05-23 18:37:59 -0400

    refactored

--- BluePhone/Views/ReceiveCallView.swift
+++ BluePhone/Views/ReceiveCallView.swift
@@ -9,12 +9,23 @@ import SwiftUI
 
 struct ReceiveCallView: View {
     var body: some View {
-        Text("Voicemail")
+        NavigationStack {
+            List {
+                Text("HI")
+            }.navigationTitle("Receive Call")
+                .listStyle(PlainListStyle())
+        }
     }
 }
 
-struct VoicemailView_Previews: PreviewProvider {
+struct ReceiveCallView_Previews: PreviewProvider {
     static var previews: some View {
         ReceiveCallView()
     }
 }
+
+//navigation structure
+//show recording options 1.
+//ring and receiving a call view 2.
+//aceept call and show calling session view 3.
+//end call returns to recording options screen
