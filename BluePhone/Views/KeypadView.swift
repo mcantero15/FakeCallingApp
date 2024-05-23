44ba4d95f4183f0ef2c3ef7568e73e1cf72056c3
mcantero15 <mariana.a.cantero15@gmail.com>
2024-05-23 18:28:50 -0400

    functioning key pad view for user input

--- BluePhone/Views/KeypadView.swift
+++ BluePhone/Views/KeypadView.swift
@@ -8,31 +8,73 @@
 import SwiftUI
 
 struct KeypadView: View {
+    //@Environment(\.action) var action //access the environment value, in this case var action(from extension EnvironmentValues)
+    @State var input: String = ""
+    
     var body: some View {
-        GeometryReader { geometry in
-            VStack {
-                
-                Text("Recording 1")
-                    .foregroundColor(.black)
-                    .lineLimit(1)
-                
-                VStack(spacing: 10) {
-                    PhoneGridRow(labels: ["1", "2", "3"])
-                    PhoneGridRow(labels: ["4", "5", "6"])
-                    PhoneGridRow(labels: ["7", "8", "9"])
-                    PhoneGridRow(labels: ["*", "0", "#"])
+        
+        VStack(){
+                Spacer()
+//                TextField("", text: $input).textFieldStyle(.plain)
+                Text(input).font(.largeTitle)
                     
-                }
-                .background(Color.red).frame(height: geometry.size.height * 0.9)
-                .padding()
+                Grid{
+                        PhoneGridRow(labels: ["1", "2", "3"])
+                        PhoneGridRow(labels: ["4", "5", "6"])
+                        PhoneGridRow(labels: ["7", "8", "9"])
+                        PhoneGridRow(labels: ["*", "0", "#"])
+                }.environment(\.action, numberPadAction(_:)).padding()
+            HStack() {
+                Spacer()
+                Spacer()
+                Button(action: {
+                    //call button with entered number
+                    let url = URL(string: "tel:\(input)")
+                    if UIApplication.shared.canOpenURL(url!)
+                    {
+                        UIApplication.shared.open(url!, options: [:], completionHandler: nil)
+                    }
+                }, label: {
+                    ZStack {
+                        Circle().fill(Color(UIColor.blue)).frame(width: 80, height: 80)
+                        Image(systemName: "phone.fill")
+                            .foregroundColor(Color.white)
+                            .font(.title)
+                    }
+                }).padding(.leading, 5.0)
                 
-            }.background(Color.green)
-        }
+                Button(action: {
+                    //delete button
+                    if !input.isEmpty
+                    {
+                        input.removeLast()
+                    }
+                }, label: {
+                    Text(verbatim: "\u{232B}")
+                        .foregroundStyle(Color(UIColor.black)).font(.largeTitle)
+                }).padding()
+                Spacer()
+            }
+            Spacer()
+                
+        }.padding()
+        
+        
     }
+    
+    func numberPadAction(_ key: String){
+        input += key
+    }
+
 }
 
+
+
 struct KeypadView_Previews: PreviewProvider {
+
     static var previews: some View {
-        KeypadView()
+        TabView {
+            KeypadView(input: "856")
+        }
     }
 }
